44f3b6d82e13412f31a723fbda488530394ba006
sa6yrov <sabyrov.aidin@gmail.com>
2020-05-08 14:30:40 +0600

    first commit

---
+++ src/main/java/kg/sabyrov/terrafit/TerrafitApplication.java
@@ -0,0 +1,13 @@
+package kg.sabyrov.terrafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrafitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerrafitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/sabyrov/terrafit/TerrafitApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.sabyrov.terrafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerrafitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
